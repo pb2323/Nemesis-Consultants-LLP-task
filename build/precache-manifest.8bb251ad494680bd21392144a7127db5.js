cc25a52a704d3efbc3b5cca3e81327a1662cd8ac
Puneet <pbajaj0023@gmail.com>
2020-09-27 01:19:20 +0530

    Build file added

---
+++ build/precache-manifest.8bb251ad494680bd21392144a7127db5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2fb7503b2defd2cbdc2ded11ee9d464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "047815197b78603ec806",
+    "url": "/static/css/main.d4ce470a.chunk.css"
+  },
+  {
+    "revision": "5b38bc9614f22d3bc659",
+    "url": "/static/js/2.ac4f26b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ac4f26b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047815197b78603ec806",
+    "url": "/static/js/main.9958b7be.chunk.js"
+  },
+  {
+    "revision": "0f9476f69e2731bbc064",
+    "url": "/static/js/runtime-main.91bdc04b.js"
+  }
+]);
